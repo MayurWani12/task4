572b2cae588e5d01ad362b6125ec0614fd582228
MayurWani12 <mayurw4347@gmail.com>
2024-10-07 17:06:23 +0530

    commit

---
+++ KALPESH/src/COM/KALPEE/KAP/DUPLICATE.java
@@ -0,0 +1,35 @@
+package COM.KALPEE.KAP;
+
+import java.util.Scanner;
+
+public class DUPLICATE {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+ 
+		
+		System.out.println("enter the size of aaray");
+		
+		int size=sc.nextInt();
+		
+		System.out.println("eneter the element"+size);
+		
+
+		
+		for (int i=0;i<size;i++) {
+			
+			int no=sc.nextInt();
+			
+		break;
+			
+		
+		
+		}
+		
+	
+		
+		
+	}
+	
+}
